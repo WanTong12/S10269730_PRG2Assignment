d7d4cf6fe38ddde522f87b624a169c8113a3f98e
Wan Tong Chong <s10267930@connect.np.edu.sg>
2025-01-14 14:12:49 +0800

    Boarding Gate Class

--- BoardingGate.cs
+++ BoardingGate.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace PRG2_T13_08
 {
-    internal class BoardingGate
+    class BoardingGate
     {
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWTT { get; set; }
+        public Flight Flight { get; set; }
+
+        public BoardingGate() { }
+
+        public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT, Flight flight)
+        {
+            GateName = gateName;
+            SupportsCFFT = supportsCFFT;
+            SupportsDDJB = supportsDDJB;
+            SupportsLWTT = supportsLWTT;
+            Flight = flight;
+        }
+
+        public double CalculateFees()
+        {
+            return;
+        }
+
+        public override string ToString()
+        {
+            return "Gate Name: " + GateName + "\tSupports CFFT: " + SupportsCFFT + "\tSupports DDJB: " + SupportsDDJB + "\tSupports LWTT: " + SupportsLWTT + "\tFlight: " + Flight;
+        }
     }
 }
