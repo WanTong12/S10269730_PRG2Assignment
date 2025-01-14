8d24ab7e6e0fa74de176a618ef8873aad5cc7469
Wan Tong Chong <s10267930@connect.np.edu.sg>
2025-01-14 13:47:40 +0800

    Flight class

--- Flight.cs
+++ Flight.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace PRG2_T13_08
 {
-    internal class Flight
+    abstract class Flight
     {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+
+        public Flight() { }
+        public Flight(string fn, string o, string d, DateTime e, string s)
+        {
+            FlightNumber = fn;
+            Origin = o;
+            Destination = d;
+            ExpectedTime = e;
+            Status = s;
+        }
+
+        public abstract double CalculateFees();
+
+        public override string ToString()
+        {
+            return "FlightNo: " + FlightNumber + "\tOrigin: " + Origin + "\tDestination: " + Destination + "\tExpected Time: " + ExpectedTime + "\tStatus: " + Status;
+        }
     }
 }
--- NORMFlight.cs
+++ NORMFlight.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace PRG2_T13_08
 {
-    internal class NORMFlight
+    class NORMFlight : Flight
     {
+        public NORMFlight():base() { }
+        public NORMFlight(string fn, string o, string d, DateTime e, string s) : base(fn, o, d, e, s) { }
+
+        public double CalculateFees()
+        {
+            return;
+        }
     }
 }
