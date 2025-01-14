62f6c64f682eb0d9493c1153c9307996a48a8c56
Wan Tong Chong <s10267930@connect.np.edu.sg>
2025-01-14 14:07:49 +0800

    AIrline Class

--- Airline.cs
+++ Airline.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace PRG2_T13_08
 {
-    internal class Airline
+    class Airline
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+
+        public Airline() { }
+
+        public Airline(string n, string c, Dictionary<string, Flight> f)
+        {
+            Name = n;
+            Code = c;
+            Flights = f;
+        }
+
+        public bool AddFlight(Flight f)
+        {
+            return;
+        }
+
+        public double CalculateFee()
+        {
+            return;
+        }
+
+        public bool RemoveFlight(Flight f)
+        {
+            return;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\tCode: " + Code;
+        }
     }
 }
