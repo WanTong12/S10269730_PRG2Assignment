e7ed7c44e0d5643f3135d51250daba4a00115183
Wan Tong Chong <s10267930@connect.np.edu.sg>
2025-01-15 20:02:02 +0800

    DisplayMenu()

--- Program.cs
+++ Program.cs
@@ -6,4 +6,9 @@ internal class Program
     {
         Console.WriteLine("Johnny Johnny yes PAPA");
     }
+
+    static void DisplayMenu()
+    {
+        Console.WriteLine("=============================================\r\nWelcome to Changi Airport Terminal 5\r\n=============================================\r\n1. List All Flights\r\n2. List Boarding Gates\r\n3. Assign a Boarding Gate to a Flight\r\n4. Create Flight\r\n5. Display Airline Flights\r\n6. Modify Flight Details\r\n7. Display Flight Schedule\r\n0. Exit");
+    }
 }
