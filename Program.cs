a9c078f905e0781a6ebdb53b29f5effa4a4f8499
Wan Tong Chong <s10267930@connect.np.edu.sg>
2025-02-02 02:05:12 +0800

    update

--- Program.cs
+++ Program.cs
@@ -189,11 +189,13 @@ internal class Program
             }
 
             flightDict.Add(fn, f); //Add object to flight dictionary
-
+            string acode = fn.Split(" ")[0];
+            Airline a = airlineDict[acode];
+            a.AddFlight(f);
         }
 
         Console.WriteLine("Loading Flights...");
-        Console.WriteLine("{0} Flights Loaded!", flightDict.Count); // -1 bcos of header
+        Console.WriteLine("{0} Flights Loaded!", flightDict.Count); 
     }
 
 
@@ -754,7 +756,8 @@ internal class Program
                 else if (f is CFFTFlight) { Console.WriteLine("Special Request Code: CFFT"); }
                 else if (f is DDJBFlight) { Console.WriteLine("Special Request Code: DDJB"); }
                 else if (f is LWTTFlight) { Console.WriteLine("Special Request Code: LWTT"); }
-                string gName = 
+                string gName = "Unassigned"; 
+                bool found = false;
                 foreach (BoardingGate b in boardingGateDict.Values) // loops through all the values in boardingGateDict to retrieve and display the updated boarding gate
                 {
                     if (b.Flight != null && b.Flight.FlightNumber == flightNo)
@@ -766,7 +769,7 @@ internal class Program
                     }
                 }
                 // if found is false, it means that the flight is not assigned to any boarding gate and it displays this message
-                if (!found) { Console.WriteLine("Boarding Gate: Unassigned"); }
+                if (!found) { Console.WriteLine("Boarding Gate: {0}", gName); }
 
             }
             else if (option == 2) // if user chooses to delete a flight
@@ -776,7 +779,7 @@ internal class Program
                 if (confirmation.ToUpper() == "Y")
                 {
                     flightDict.Remove(flightNo); // removing flight from flight dictionary 
-                    boardingGateDict.RemoveValue(flightNo) // remove flight from the boarding gate dict's value
+                    boardingGateDict.RemoveValue(flightNo); // remove flight from the boarding gate dict's value
                     Console.WriteLine("Flight deleted successfully!");
                 }
             }
